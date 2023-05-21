ba4aa6e8de9a3a3d2627200590a300b639e6bb77
ninnh <phoanninhninh@gmail.com>
2023-05-22 06:54:47 +0700

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -61,3 +61,88 @@ const activeElemOnScroll = function () {
 }
 
 addEventOnElem(window, "scroll", activeElemOnScroll);
+
+
+
+/**
+ * product detail 
+ */
+
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav = document.getElementById('navbar-items');
+
+if (bar){
+    bar.addEventListener('click', () =>{
+        nav.classList.add('active')
+    })
+}
+
+if (close){
+    close.addEventListener('click',() =>{
+        nav.classList.remove('active');
+    })
+}
+
+function changePrice() {
+    var bookType = document.getElementById("book-type").value;
+    var bookPrice = document.getElementById("price");
+    if (bookType === "hardback") {
+      bookPrice.innerHTML = "$45.00";
+    } else {
+      bookPrice.innerHTML = "$50.00";
+    }
+  }
+  
+
+var bigImg = document.getElementById("main-img");
+var smallImg = document.getElementsByClassName("smalImg")
+
+smallImg[0].onclick = function(){
+    bigImg.src = smallImg[0].src;
+}
+smallImg[1].onclick = function(){
+    bigImg.src = smallImg[1].src;
+}
+smallImg[2].onclick = function(){
+    bigImg.src = smallImg[2].src;
+}
+
+function rst(){
+    document.getElementById('form').reset();
+}
+
+
+
+/**
+ * budget filter
+ */
+
+let minValue = document.getElementById("min-value");
+let maxValue = document.getElementById("max-value");
+
+function validateRange(minPrice, maxPrice) {
+  if (minPrice > maxPrice) {
+
+    // Swap to Values
+    let tempValue = maxPrice;
+    maxPrice = minPrice;
+    minPrice = tempValue;
+  }
+
+  minValue.innerHTML = "$" + minPrice;
+  maxValue.innerHTML = "$" + maxPrice;
+}
+
+const inputElements = document.querySelectorAll("input");
+
+inputElements.forEach((element) => {
+  element.addEventListener("change", (e) => {
+    let minPrice = parseInt(inputElements[0].value);
+    let maxPrice = parseInt(inputElements[1].value);
+
+    validateRange(minPrice, maxPrice);
+  });
+});
+
+validateRange(inputElements[0].value, inputElements[1].value);
